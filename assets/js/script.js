de14ac2ad8b7d3df7711bebf0e97e479c6ba985d
igor dias da silva <00001114512266sp@al.educacao.sp.gov.br>
2025-08-22 12:10:08 -0300

    Criamos os eventos nos botões de login e register para abir e fechar modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
+const modalLogin = document.querySelector("#modal-login");
+const modalRegister = document.querySelector("#modal-register");
+const btnCancelLogin =document.querySelector("#btn-cancel-login");
+const btnCancelRegister =document.querySelector("#btn-cancel-Register");
+
 
 btnLogin.addEventListener("click", (e) => {
-    console.log("Cliquei no botão login");
-    
+   modalLogin.classList.remove("hide-modal");
+   modalLogin.classList.add("show-modal");
+})
+
+btnRegister.addEventListener("click", (e) => {
+    modalRegister.classList.remove("hide-modal");
+    modalRegister.classList.add("show-modal");
+})
+btnCancelLogin.addEventListener("click", (e) => {
+    modalLogin.classList.add("hide-modal");
+    modalLogin.classList.remove("show-modal");
+})
+
+btnCancelRegister.addEventListener("click", (e) => {
+    modalRegister.classList.remove("show-modal");
+    modalRegister.classList.add("hide-modal");
 })
