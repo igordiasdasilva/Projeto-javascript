0097ae6a662d38b2690d87314506f54af0214415
igor dias da silva <00001114512266sp@al.educacao.sp.gov.br>
2025-09-05 12:01:30 -0300

    Criamos uma função de  calcular

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,107 @@ const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
 const modalLogin = document.querySelector("#modal-login");
 const modalRegister = document.querySelector("#modal-register");
-const btnCancelLogin =document.querySelector("#btn-cancel-login");
-const btnCancelRegister =document.querySelector("#btn-cancel-register");
-const btnLoginOpen = document.querySelector("#btn-login");
+const btnCancelLogin = document.querySelector("#btn-cancel-login");
+const btnCancelRegister = document.querySelector("#btn-cancel-register");
+const btnLoginOpen = document.querySelector("#btn-login-open");
 const inputEmail = document.querySelector("#email");
-const resultado = document.querySelector("#resultado")
+const inputSenha = document.querySelector("#senha");
+const resultado = document.querySelector("#resultado");
 
-function soma(num01, num02) {
-    const somar = num01 + num02;
-    console.log(somar); 
+const pegar_valor = (valor) => {
+  resultado.innerHTML = `
+    <div class="">
+    <h1>${valor}</h1>
+    </div>
+  `;
 }
 
-soma(8, 6)
+const calcular = (valor01, valor02) => {
+  const soma = Number(valor01) + Number(valor02);
+  const subtrair = valor01 - valor02;
+  const multiplicar = valor01 * valor02;
+  const dividir = valor01 / valor02;
 
-// arrow function
+console.log(typeof valor01);
+
+  return resultado.innerHTML = `
+    <div class="calcular">
+      <div class="soma">${soma}</div>
+      <div class="subtrair">${subtrair}</div>
+      <div class="multiplicar">${multiplicar}</div>
+      <div class="dividir">${dividir}</div>
+    </div>
+  `
+}
+
+function soma(num01, num02) {
+  const somar = num01 + num02;
+  console.log(somar);
+}
+
+soma(5, 8)
+
+// arrow function 
 const multiplicar = (num01, num02) => {
-    return num01 * num02
+  return num01 * num02
 }
 
 console.log(multiplicar(5, 8.9));
 
-
-const subtrair = (num01, num02) => {
-    return num01 - num02
-}
-console.log(subtrair(7, 9-4));
-
-
-
-// addEventLister => Escuta o evento de click
+// addEventListener => Escuta o evento de click
 // e executa a função
+// eventos => click, focus, change, input, keypress, keyup, keydown
 btnLogin.addEventListener("click", (e) => {
-   modalLogin.classList.remove("hide-modal");
-   modalLogin.classList.add("show-modal");
+  modalLogin.classList.remove("hide-modal");
+  modalLogin.classList.add("show-modal");
 })
 
 btnRegister.addEventListener("click", (e) => {
-    modalRegister.classList.remove("hide-modal");
-    modalRegister.classList.add("show-modal");
-})
+  modalRegister.classList.remove("hide-modal");
+  modalRegister.classList.add("show-modal");
+});
+
 btnCancelLogin.addEventListener("click", (e) => {
-    modalLogin.classList.add("hide-modal");
-    modalLogin.classList.remove("show-modal");
-})
+    e.preventDefault();
+  modalLogin.classList.remove("show-modal");
+  modalLogin.classList.add("hide-modal");
+  inputEmail.value = "";
+  inputSenha.value = "";
+  resultado.innerHTML = "";
+  inputEmail.style.borderBottom = "0.5rem solid rgb(0, 179, 255)";
+  inputSenha.style.borderBottom = "0.5rem solid rgb(0, 179, 255)";
+});
 
 btnCancelRegister.addEventListener("click", (e) => {
-    modalRegister.classList.remove("show-modal");
-    modalRegister.classList.add("hide-modal");
-})
-inputEmail.addEventListener("focus", () => {
-    inputEmail.style.borderBottom = "0.5rem solid rgb(255, 0, 0 )"
-})
+    e.preventDefault();
+  modalRegister.classList.remove("show-modal");
+  modalRegister.classList.add("hide-modal");
+});
 
 // inputEmail.addEventListener("input", () => {
-    // resultado.innerHTML = inputEmail.value
+//   resultado.innerHTML = inputEmail.value
 // })
 
+inputEmail.addEventListener("focus", () => {
+  inputEmail.style.borderBottom = "0.5rem solid rgb(255, 0, 0)";
+});
+
+inputSenha.addEventListener("focus", () => {
+  inputSenha.style.borderBottom = "0.5rem solid rgb(255, 0, 0)";
+});
+
+btnLoginOpen.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = inputEmail.value;
+    const Senha = inputEmail.value;
+    calcular(email, senha);
+    resultado.classList.add("resultado_calcular");
+    
+
+  });
+inputEmail.addEventListener("focusout", () => {
+  inputEmail.style.borderBottom = "0.5rem solid rgb(0, 179, 255)";
+});
+inputSenha.addEventListener("focusout", () => {
+  inputSenha.style.borderBottom = "0.5rem solid rgb(0, 179, 255)";
+});
